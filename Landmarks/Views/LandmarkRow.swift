5c828bc117e1f51b9fb6be586b8ec7d071b52d3a
daviidbartlett <daviidbartlett@gmail.com>
2022-03-21 15:49:45 +0000

    Add LandmarkRow view

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,25 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by David Bartlett on 21/03/2022.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    var body: some View {
+        HStack{
+            landmark.image.resizable().frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
