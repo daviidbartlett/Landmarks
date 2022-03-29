1b3723e3b21f0a2310d6ccb7f233671200779a89
daviidbartlett <daviidbartlett@gmail.com>
2022-03-29 14:45:36 +0100

    add favourite filter option

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -14,6 +14,10 @@ struct LandmarkRow: View {
             landmark.image.resizable().frame(width: 50, height: 50)
             Text(landmark.name)
             Spacer()
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
@@ -22,7 +26,7 @@ struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
         
         Group {
-            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[2])
             LandmarkRow(landmark: landmarks[1])
                
         } .previewLayout(.fixed(width: 300, height: 70))
